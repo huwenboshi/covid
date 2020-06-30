4fe85e629ee71f79848f5b99969c4cb8d0a75b87
Huwenbo Shi <hshi@hsph.harvard.edu>
2020-06-30 10:56:47 -0400

    backup corchisq

--- analysis/corr_chisq_seg.py
+++ analysis/corr_chisq_seg.py
@@ -4,7 +4,7 @@
 import numpy as np
 import pandas as pd
 import argparse, math, sys, logging, time
-from src.estimation import *
+from scipy import stats
 
 # main function
 def main():
@@ -16,64 +16,39 @@ def main():
     argmap = check_command_line(args)
 
     # load frequency file
+    start_chrom, stop_chrom = argmap['use-chrom']
     freq = load_frqfile(args.frqfile, start_chrom, stop_chrom)
+    freq = freq[freq['MAF'] > 0.05].reset_index(drop=True)
 
     # load seg annotations
-    annot_snps,all_seg_annot,seg_annot_mat = load_annot(argmap['seg-annot'],
-        start_chrom, stop_chrom)
-    annot_idx = intersect_snp_estimation(annot_snps['SNP'], freq, 0.05)
+    annot = load_annot(argmap['seg-annot'], start_chrom, stop_chrom)
+    annot = annot.merge(freq, on=['SNP'])
+    annot = annot.reset_index(drop=True)
 
-def load_annot(prefix_list, start_chrom, end_chrom,
-    annot_start_idx=4, snp_idx=2):
-    """
-    Load annotation of all chromosomes
-    """
+    # load gwas sumstat
+    sumstat = pd.read_table(argmap['sumstat'], delim_whitespace=True)
+    sumstat['CHISQ'] = np.square(sumstat['Z'])
 
-    # load all snps snps
-    all_snp = []
-    for i in xrange(start_chrom, end_chrom+1):
-        prefix = prefix_list[0]
+    # merge with annotation
+    sumstat_annot = sumstat.merge(annot, on=['SNP'])
+    sumstat_annot = sumstat_annot.reset_index(drop=True)
+    
+    # get rankr and se
+    chisq = sumstat_annot['CHISQ'].values
+    seg = sumstat_annot['{}.bed'.format(argmap['tissue'])].values
+
+# get 
+
+# load annotation
+def load_annot(prefix, start_chrom, end_chrom):
+
+    annot = pd.DataFrame()
+    for i in range(start_chrom, end_chrom+1):
         filename = '{}{}.annot.gz'.format(prefix, i)
-        tbl = pd.read_table(filename, delim_whitespace=True, engine='c',
-            na_filter=False, memory_map=True, usecols=['SNP'])
-        all_snp.append(tbl)
-    all_snp = pd.concat(all_snp, axis=0, ignore_index=True)
-    tot_nsnp = all_snp.shape[0]
+        tmp = pd.read_table(filename, delim_whitespace=True)
+        annot = pd.concat([annot, tmp], ignore_index=True)
 
-    # get annotations
-    all_annot = []
-    annot_list = []
-    for i in xrange(len(prefix_list)):
-        prefix = prefix_list[i]
-        filename = '{}{}.annot.gz'.format(prefix, start_chrom)
-        with gzip.open(filename) as f:
-            line = f.readline().strip()
-            tmp = line.split()[annot_start_idx:]
-            all_annot += tmp
-            annot_list.append(tmp)
-    tot_nannot = len(all_annot)
-    
-    # load the annot into memory
-    all_annot_mat = np.zeros((tot_nsnp, tot_nannot), dtype=np.float32)
-    idx_c = 0
-    for k in xrange(len(prefix_list)):
-        prefix = prefix_list[k]
-        idx_r = 0
-        for i in xrange(start_chrom, end_chrom+1):
-            filename = '{}{}.annot.gz'.format(prefix, i)
-            tmp = annot_list[k]
-            dt_load = dict(zip(tmp, [np.float32]*len(tmp)))
-            tbl = pd.read_table(filename, delim_whitespace=True, engine='c',
-                na_filter=False, memory_map=True, usecols=tmp, dtype=dt_load)
-            nrow, ncol = tbl.shape
-            all_annot_mat[idx_r:idx_r+nrow,idx_c:idx_c+ncol] = tbl[tmp].values
-            idx_r += nrow
-        idx_c += ncol
-    
-    # convert annot to data frame
-    all_annot = pd.DataFrame({'ANNOT': all_annot})
-
-    return all_snp, all_annot, all_annot_mat
+    return annot
 
 # intersect snp
 def intersect_snp_estimation(annot_snps, freq, min_maf):
@@ -84,7 +59,7 @@ def intersect_snp_estimation(annot_snps, freq, min_maf):
 def load_frqfile(frqfile_fnm, start_chrom, stop_chrom):
     
     all_frq = []
-    for i in xrange(start_chrom, stop_chrom+1):
+    for i in range(start_chrom, stop_chrom+1):
         all_frq.append(pd.read_table('{}{}.frq'.format(frqfile_fnm,i),
             delim_whitespace=True))
     
@@ -125,14 +100,20 @@ def get_command_line():
     parser = argparse.ArgumentParser(description='Get correlation between'
         'chisq statistics and SEG annotation')
 
+    parser.add_argument('--tissue', dest='tissue', type=str,
+        required=False, help='Tissue name')
+
+    parser.add_argument('--sumstat', dest='sumstat', type=str,
+        required=False, help='Baseline annotation file')
+
     parser.add_argument('--seg-annot', dest='seg-annot', type=str,
-        required=False, nargs='+', help='Baseline annotation file')
+        required=False, help='SEG annotation file')
 
     parser.add_argument('--use-chrom', dest='use-chrom', type=int,
         required=False, default=[1, 22], nargs=2,
-        help='Specific which chromosomes to use')
+        help='Specify which chromosomes to use')
 
-    parser.add_argument('--frqfile', dest='frqfile', type=str, nargs=2,
+    parser.add_argument('--frqfile', dest='frqfile', type=str,
         required=False, help='Prefix of the frequency files')
 
     parser.add_argument('--out', dest='out', type=str,
